5418139422114516192cedce1dfe292795927f0a
boramkim <boramyyo@gmail.com>
2019-01-15 22:06:15 +0900

    using eslint with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,15 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const eslint = require('gulp-eslint');
 // 걸프 의존성 작성
 
 gulp.task('default', function (cb) {
   // 걸프가 실행할 작업을 여기 쓴다
+
+  // eslint를 실행
+  gulp.src(['es6/**/*.js', 'public/es6/**/*.js'])
+    .pipe(eslint())
+    .pipe(eslint.format());
 
   // 노드(서버) 소스
   gulp.src('es6/**/*.js')
