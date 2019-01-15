a9731c32e77e738ec2e88a2a95c908a62953e4cf
boramkim <boramyyo@gmail.com>
2019-01-15 22:19:11 +0900

    corrected as lint command

--- es6/test.js
+++ es6/test.js
@@ -1,19 +1,19 @@
 'use strict';
-// 서버코드 !
 
-// es6 기능 : 블록 스코프 변수 선언
 const sentences = [
+  // es6 기능 : 블록 스코프 변수 선언
   { subject: 'javascript', verb: 'is', object: 'great' },
   { subject: 'giraffes', verb: 'are', object: 'tall' },
 ];
 
 // es6 기능 : 객체 분해
-function say({ subject, verb, object}) {
+function say({ subject, verb, object }) {
   // es6 기능 : 백틱(`)을 사용한 템플릿 문자열
   console.log(`${subject} ${verb} ${object}`);
 }
 
 // es6 기능 : for of
-for(let s of sentences) {
-  say(s);
-}
+sentences.every(s => say(s));
+// for (const s of sentences) {
+//   say(s);
+// }
